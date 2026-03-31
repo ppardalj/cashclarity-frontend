5372558b5b3eaef4995a2afbf1317cbcaa85521a
Pedro Pardal <ppardalj@gmail.com>
2026-04-01 00:30:13 +0200

    remove unnecessary stuff

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(({mode}) => {
   return {
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL || env.APP_URL),
     },
     resolve: {
